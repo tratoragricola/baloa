1f29e8db1832a9715c26ba25443a2d51fd740eb0
tratoragricola <98431261+tratoragricola@users.noreply.github.com>
2022-06-14 19:58:38 -0300

    Add files via upload

---
+++ central.js
@@ -0,0 +1,32 @@
+function preload(){
+
+ ceu1 = loadImage("images/ceu_ceu.png")
+ ceu2 = loadImage("images/ceu.png")
+ ceu3 = loadImage("images/ceu_amarelo.png")
+ ceu4 = loadImage("images/ceu_rosa.png")
+ ceu5 = loadImage("images/sunset.png")
+ ceu6 = loadImage("images/por_do_sol.png")
+ 
+}
+function Controls(){
+
+  velocity.y = 3
+  velocity.x = 2
+
+
+
+ }
+function setup(){
+createCanvas(windowWidth,windowHeight)
+
+} 
+
+function zoomout(){
+ 
+
+}
+
+function draw(){
+
+background(ceu5)
+}
